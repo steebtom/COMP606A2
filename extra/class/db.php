736b1ed0a5f4575c8d5c068ae97af5b8f6f620b4
MariaSusainathan <47832619+MariaSusainathan@users.noreply.github.com>
2019-05-31 12:52:20 +0530

    class database
    class database

---
+++ extra/class/db.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database
+{
+  public $conn;
+
+  public function  __construct()
+  {
+    
+    $username = "root";
+    $password = "";
+    $servername = "localhost";
+
+    try 
+    {
+
+      $this->conn = new PDO("mysql:host=$servername;dbname=oop", $username, $password);
+    
+      $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    }
+
+    catch(PDOException $e)
+    {
+      echo "Connection failed: " . $e->getMessage();
+    }
+  }
+
+
+}
