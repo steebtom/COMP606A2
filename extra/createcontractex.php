d8bc1ad69da34253caf3b099dff1fb017838f0f2
steeb-wintec <steeb.webdev@gmail.com>
2019-06-12 22:51:41 +1200

    Create job feature and features for contracts

--- extra/createcontractex.php
+++ extra/createcontractex.php
@@ -1,15 +1,11 @@
-
 <?php
-
-
 include_once("class/user.php");
 include_once("class/contracts.php");
 
 session_start();
 
-
 if(isset($_POST['createcontractbtn']))
-{
+{   
         $tcost = $_POST['tcost'];
         $labour = $_POST['labour'];
         $material = $_POST['material'];
@@ -17,78 +13,20 @@ if(isset($_POST['createcontractbtn']))
         $sjid = $_SESSION['sjid'];
         $sesuser = $_SESSION['uid'];
 
-
-
     if((empty($tcost)) || (empty($labour)) || (empty($material)) || (empty($date)))
     {
         header("tcostation: ../jobdetails.php?error=emptyfields&username=email=");
         exit();
     }
-
     
     else
     {
-
-        
-        // $pw =  password_hash($pwd,PASSWORD_BCRYPT);
-        // $pw =  password_hash($pwd, PASSWORD_DEFAULT);
-
-        
-
-        // $connection = new Database();
         if(isset($_SESSION['uid']))                                      //Session Variable Check and Printing Page Layout
             { 
-                $connection = new Database();
-                // $sesvar = $_SESSION['user'];
-                // $sesutype = $sesvar->getuType();
-                // echo $sesutype;
-                // $sesuser = $_SESSION['user'];
-                // $sesuserid = $sesuser->getuserId();
-
-                $query = "INSERT INTO CONTRACT VALUES ('', '$tcost', '$labour', '$material', '$date', '$sjid', '$sesuser')";
-                $prepCheck = $connection->conn->prepare($query);
-                $res = $prepCheck->execute();
-
-                // $res = $prep->execute();
-                if($res)
-                    {
-                        echo "<script>alert('Contract Created Successfully'); </script>";
-                        // echo "<script>window.top.tcostation='index.php'</script>";
-                        // echo "<script>window.tcostation.href='index.php'</script>";
-                        header("location:../jobdetails.php?contractcreated");
-
-                    }
-                else
-                    {
-                        echo "<script>alert('Couldnt Create the Contract'); </script>";
-                    }
-            }
-        
-        // if($prepCheck->fetchColumn())
-        // {
-        //     echo "<script>alert('Username or email already exists'); </script>";
-        // //   echo "<script>window.tcostation.href='index.php'</script>";
-        //     header("tcostation:../signup.php?error=emailuserexist");
-        // }
-        // else
-        // {
-
-
-        //     $query = "INSERT INTO users VALUES ('','$uname','$fname','$email','$pwd','$utype')";
-
-        //     $prep = $connection->conn->prepare($query);
-        //     $res = $prep->execute();
-        //     if($res)
-        //     {
-        //         echo "<script>alert('Registered Successfull'); </script>";
-        //         // echo "<script>window.top.tcostation='index.php'</script>";
-        //         // echo "<script>window.tcostation.href='index.php'</script>";
-        //         header("tcostation:../index.php?signupsuccess");
-
-        //     }
-        // }
-    }
-    
+                $contract = new Contracts();
+                $result = $contract->createcontract($tcost, $labour, $material, $date, $sjid, $sesuser);
+            }   
+    }    
 }
 
 else
